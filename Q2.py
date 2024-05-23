7cd54c682c3c18203bac5df554166d92863a1bca
Essamlhem3355 <essamlhem33@gmail.com>
2024-05-23 14:35:43 +0300

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,20 +1,20 @@
 def binary_to_decimal(binary):
     try:
-        decimal = int(binary, 2)
-        return decimal
+        d = int(binary, 2)
+        return d
     except ValueError:
         return None
 while True:
-    binary_input = input("Enter a binary number: ")
+    binary_number = input("Enter a binary number: ")
 
-    if all(char in '01' for char in binary_input):
+    if all(char in '01' for char in binary_number):
         break
     else:
         print("Invalid input.")
 
-decimal_result = binary_to_decimal(binary_input)
+d_result = binary_to_decimal(binary_number)
 
-if decimal_result is not None:
-    print(f"The decimal equivalent of {binary_input} is: {decimal_result}")
+if d_result is not None:
+    print(f"The decimal equivalent of {binary_number} is: {d_result}")
 else:
     print("Invalid binary number.")
